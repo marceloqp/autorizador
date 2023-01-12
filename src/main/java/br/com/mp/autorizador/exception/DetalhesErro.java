47969c9f9cbb935f1d026419cef6304893dc9ac0
Marcelo Quadrelli Pinheiro <marceloquadrelli@ext.cvccorp.com.br>
2023-01-12 00:17:52 -0300

    feat:criado mapeamento de exception.

---
+++ src/main/java/br/com/mp/autorizador/exception/DetalhesErro.java
@@ -0,0 +1,22 @@
+package br.com.mp.autorizador.exception;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+
+@Data
+public final class DetalhesErro implements Serializable {
+
+    private String status;
+    private String message;
+
+    public DetalhesErro(String mensagemErro) {
+        message = mensagemErro;
+    }
+
+    public DetalhesErro(HttpStatus status, String message) {
+        this.message = message;
+        this.status = status.toString();
+    }
+}
---
+++ src/main/java/br/com/mp/autorizador/exception/GenericExceptionHandler.java
@@ -0,0 +1,52 @@
+package br.com.mp.autorizador.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseStackTrace;
+
+@ControllerAdvice
+@Slf4j
+public class GenericExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({GenericRestException.class})
+    public ResponseEntity<Object> handleOxyExcpetion(GenericRestException ex, WebRequest request) {
+        log.info("Problema na requisição. Message: " + StringUtils.stripToEmpty(ex.getMessage()), ex);
+        return ResponseEntity.status(ex.getHttpStatus()).body(ex.getDetalhesErro());
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request) {
+        log.error("Erro na requisição. Message: " + StringUtils.stripToEmpty(ex.getMessage()), ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new DetalhesErro(HttpStatus.INTERNAL_SERVER_ERROR,
+                        Arrays.toString(getRootCauseStackTrace(ex))));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleValidationExceptions(ConstraintViolationException ex) {
+        log.error("Erro na requisição. Message: " + StringUtils.stripToEmpty(ex.getMessage()), ex);
+
+        Set<String> errors = new HashSet<>();
+        ex.getConstraintViolations().forEach(error -> {
+            String errorMessage = error.getMessage();
+            errors.add(errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new DetalhesErro(HttpStatus.INTERNAL_SERVER_ERROR,
+                        errors.toString().replaceAll("[\\[\\]]", "")));
+    }
+
+}
